3e13db66f9a1cdc434145b85c41577eb93737486
ivanjermakov <ivanjermakov1@gmail.com>
2025-05-24 13:18:15 +0200

    Print file

--- src/main.zig
+++ src/main.zig
@@ -7,26 +7,36 @@ const nc = @cImport({
     @cInclude("ncurses.h");
 });
 
-pub fn main() !void {
-    const parser = ts.ts_parser_new();
-    const language = ts.tree_sitter_c();
-    _ = ts.ts_parser_set_language(parser, language);
-
-    const source_code = "int main() { return 0; }";
-    const tree = ts.ts_parser_parse_string(parser, null, source_code, source_code.len);
-
-    const root_node = ts.ts_tree_root_node(tree);
-    std.debug.print("Root node type: {*}\n", .{ts.ts_node_type(root_node)});
-
-    ts.ts_tree_delete(tree);
-    ts.ts_parser_delete(parser);
-}
-
 // pub fn main() !void {
-//     const win = nc.initscr();
-//     _ = win;
-//     _ = nc.printw("hello, ncurses");
-//     _ = nc.refresh();
-//     _ = nc.getch();
-//     _ = nc.endwin();
+//     const parser = ts.ts_parser_new();
+//     const language = ts.tree_sitter_c();
+//     _ = ts.ts_parser_set_language(parser, language);
+//
+//     const source_code = "int main() { return 0; }";
+//     const tree = ts.ts_parser_parse_string(parser, null, source_code, source_code.len);
+//
+//     const root_node = ts.ts_tree_root_node(tree);
+//     std.debug.print("tree: {s}\n", .{@as([*:0]u8, ts.ts_node_string(root_node))});
+//
+//     ts.ts_tree_delete(tree);
+//     ts.ts_parser_delete(parser);
 // }
+
+pub fn main() !void {
+    var args = std.process.args();
+    _ = args.skip();
+    const path = args.next() orelse return error.NoPath;
+
+    const file = try std.fs.cwd().openFile(path, .{ .mode = .read_write });
+
+    var buffer: []u8 = try std.heap.page_allocator.alloc(u8, 2048);
+    const file_len = try file.readAll(buffer);
+    const content: []u8 = buffer[0..file_len];
+
+    const win = nc.initscr();
+    _ = win;
+    _ = nc.printw(@ptrCast(content));
+    _ = nc.refresh();
+    _ = nc.getch();
+    _ = nc.endwin();
+}
