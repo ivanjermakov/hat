e1d6e20fac1f0cec7b4feaad929260e124dd4087
ivanjermakov <ivanjermakov1@gmail.com>
2025-05-29 20:24:02 +0200

    Move cursor action

---
+++ src/action.zig
@@ -0,0 +1,12 @@
+const main = @import("main.zig");
+const nc = @cImport({
+    @cInclude("ncurses.h");
+});
+
+pub fn try_move_cursor(try_cursor: main.Cursor) void {
+    const win_size = .{ .row = nc.getmaxy(nc.stdscr), .col = nc.getmaxx(nc.stdscr) };
+    if (try_cursor.row < 0 or try_cursor.row > win_size.row - 1 or
+        try_cursor.col < 0 or try_cursor.col > win_size.col - 1) return;
+    (&main.cursor).* = try_cursor;
+    main.need_redraw = true;
+}
--- src/main.zig
+++ src/main.zig
@@ -6,12 +6,22 @@ const dl = std.DynLib;
 const nc = @cImport({
     @cInclude("ncurses.h");
 });
+const action = @import("action.zig");
 
-const Buffer = std.ArrayList(Line);
+pub const Buffer = std.ArrayList(Line);
 
-const Line = std.ArrayList(u8);
+pub const Cursor = struct {
+    row: i32,
+    col: i32,
+};
+pub var cursor: Cursor = .{ .row = 0, .col = 0 };
 
-const color = enum(u8) {
+pub var need_redraw = false;
+pub var need_reparse = false;
+
+pub const Line = std.ArrayList(u8);
+
+pub const Color = enum(u8) {
     black = 1,
     white,
     red,
@@ -23,31 +33,31 @@ const color = enum(u8) {
         return @intFromFloat(f);
     }
 
-    pub fn init(self: color, r: u8, g: u8, b: u8) void {
+    pub fn init(self: Color, r: u8, g: u8, b: u8) void {
         _ = nc.init_color(@intFromEnum(self), rgb_to_curses(r), rgb_to_curses(g), rgb_to_curses(b));
     }
 };
 
-const color_pair = enum(u8) {
+pub const ColorPair = enum(u8) {
     text = 1,
     keyword,
     string,
     number,
 
-    pub fn init(self: color_pair, fg: color, bg: color) void {
+    pub fn init(self: ColorPair, fg: Color, bg: Color) void {
         _ = nc.init_pair(@intFromEnum(self), @intFromEnum(fg), @intFromEnum(bg));
     }
 
-    pub fn to_pair(self: color_pair) c_int {
+    pub fn to_pair(self: ColorPair) c_int {
         return @as(c_int, @intFromEnum(self)) * 256;
     }
 };
 
-const attr = .{
-    .text = color_pair.text.to_pair(),
-    .keyword = color_pair.keyword.to_pair() | nc.A_BOLD,
-    .string = color_pair.string.to_pair(),
-    .number = color_pair.number.to_pair(),
+const Attr = .{
+    .text = ColorPair.text.to_pair(),
+    .keyword = ColorPair.keyword.to_pair() | nc.A_BOLD,
+    .string = ColorPair.string.to_pair(),
+    .number = ColorPair.number.to_pair(),
 };
 
 fn buffer_new(content: []u8, alloc: std.mem.Allocator) !Buffer {
@@ -60,6 +70,15 @@ fn buffer_new(content: []u8, alloc: std.mem.Allocator) !Buffer {
         try lines.append(line);
     }
     return lines;
+}
+
+fn buffer_content(buffer: *const Buffer, alloc: std.mem.Allocator) ![]u8 {
+    var content = std.ArrayList(u8).init(alloc);
+    for (buffer.items) |line| {
+        try content.appendSlice(line.items);
+        try content.append('\n');
+    }
+    return content.items;
 }
 
 const Span = struct {
@@ -76,7 +95,7 @@ const SpanNodeTypeTuple = struct {
 
 fn make_spans(root_node: ts.struct_TSNode, alloc: std.mem.Allocator) !std.ArrayList(SpanNodeTypeTuple) {
     var spans = std.ArrayList(SpanNodeTypeTuple).init(alloc);
-    var cursor = ts.ts_tree_cursor_new(root_node);
+    var tree_cursor = ts.ts_tree_cursor_new(root_node);
     var node = root_node;
 
     traverse: while (true) {
@@ -88,16 +107,16 @@ fn make_spans(root_node: ts.struct_TSNode, alloc: std.mem.Allocator) !std.ArrayL
         const span = .{ .start_byte = start_byte, .end_byte = end_byte };
         try spans.append(.{ .span = span, .node_type = @constCast(node_type) });
 
-        if (ts.ts_tree_cursor_goto_first_child(&cursor)) {
-            node = ts.ts_tree_cursor_current_node(&cursor);
+        if (ts.ts_tree_cursor_goto_first_child(&tree_cursor)) {
+            node = ts.ts_tree_cursor_current_node(&tree_cursor);
         } else {
             while (true) {
-                if (ts.ts_tree_cursor_goto_next_sibling(&cursor)) {
-                    node = ts.ts_tree_cursor_current_node(&cursor);
+                if (ts.ts_tree_cursor_goto_next_sibling(&tree_cursor)) {
+                    node = ts.ts_tree_cursor_current_node(&tree_cursor);
                     break;
                 } else {
-                    if (ts.ts_tree_cursor_goto_parent(&cursor)) {
-                        node = ts.ts_tree_cursor_current_node(&cursor);
+                    if (ts.ts_tree_cursor_goto_parent(&tree_cursor)) {
+                        node = ts.ts_tree_cursor_current_node(&tree_cursor);
                     } else {
                         break :traverse;
                     }
@@ -117,20 +136,35 @@ fn init_curses() !*nc.WINDOW {
         _ = nc.start_color();
     }
 
-    color.black.init(0, 0, 0);
-    color.white.init(255, 255, 255);
-    color.red.init(255, 0, 0);
-    color.green.init(0, 255, 0);
-    color.blue.init(0, 0, 255);
+    Color.black.init(0, 0, 0);
+    Color.white.init(255, 255, 255);
+    Color.red.init(255, 0, 0);
+    Color.green.init(0, 255, 0);
+    Color.blue.init(0, 0, 255);
 
-    color_pair.text.init(color.white, color.black);
-    color_pair.keyword.init(color.red, color.black);
-    color_pair.string.init(color.green, color.black);
-    color_pair.number.init(color.blue, color.black);
+    ColorPair.text.init(Color.white, Color.black);
+    ColorPair.keyword.init(Color.red, Color.black);
+    ColorPair.string.init(Color.green, Color.black);
+    ColorPair.number.init(Color.blue, Color.black);
 
-    _ = nc.bkgd(@intCast(color_pair.text.to_pair()));
+    _ = nc.bkgd(@intCast(ColorPair.text.to_pair()));
 
     return win;
+}
+
+fn init_parser() !?*ts.TSParser {
+    const parser = ts.ts_parser_new();
+    var language_lib = try dl.open("/usr/lib/tree_sitter/c.so");
+    var language: *const fn () *ts.struct_TSLanguage = undefined;
+    language = language_lib.lookup(@TypeOf(language), "tree_sitter_c") orelse return error.NoSymbol;
+    _ = ts.ts_parser_set_language(parser, language());
+    return parser;
+}
+
+fn ts_parse(parser: ?*ts.TSParser, buffer: *const Buffer, allocator: std.mem.Allocator) !?*ts.TSTree {
+    const content = try buffer_content(buffer, allocator);
+    const tree = ts.ts_parser_parse_string(parser, null, @ptrCast(content), @intCast(content.len));
+    return tree;
 }
 
 fn redraw(buffer: *const Buffer, spans: *const std.ArrayList(SpanNodeTypeTuple)) void {
@@ -142,24 +176,24 @@ fn redraw(buffer: *const Buffer, spans: *const std.ArrayList(SpanNodeTypeTuple))
 
         for (0..line.len) |col| {
             const ch = line[col];
-            var ch_attr = attr.text;
+            var ch_attr = Attr.text;
             for (spans.items) |span| {
                 if (span.span.start_byte <= byte and span.span.end_byte > byte) {
                     if (std.mem.eql(u8, span.node_type, "return") or
                         std.mem.eql(u8, span.node_type, "primitive_type") or
                         std.mem.eql(u8, span.node_type, "#include"))
                     {
-                        ch_attr = attr.keyword;
+                        ch_attr = Attr.keyword;
                         break;
                     }
                     if (std.mem.eql(u8, span.node_type, "system_lib_string") or
                         std.mem.eql(u8, span.node_type, "string_literal"))
                     {
-                        ch_attr = attr.string;
+                        ch_attr = Attr.string;
                         break;
                     }
                     if (std.mem.eql(u8, span.node_type, "number_literal")) {
-                        ch_attr = attr.number;
+                        ch_attr = Attr.number;
                         break;
                     }
                 }
@@ -175,6 +209,20 @@ fn redraw(buffer: *const Buffer, spans: *const std.ArrayList(SpanNodeTypeTuple))
     _ = nc.refresh();
 }
 
+fn update(parser: ?*ts.TSParser, buffer: *const Buffer, allocator: std.mem.Allocator) !void {
+    const tree = try ts_parse(parser, buffer, allocator);
+    defer ts.ts_tree_delete(tree);
+    const root_node = ts.ts_tree_root_node(tree);
+    // std.debug.print("tree: {s}\n", .{@as([*:0]u8, ts.ts_node_string(root_node))});
+    const spans = try make_spans(root_node, allocator);
+    // for (spans.items) |span| {
+    //     std.debug.print("{s}\n", .{span.node_type});
+    // }
+
+    redraw(buffer, &spans);
+    _ = nc.move(@intCast(cursor.row), @intCast(cursor.col));
+}
+
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
     var args = std.process.args();
@@ -185,38 +233,36 @@ pub fn main() !void {
 
     var buf: []u8 = try allocator.alloc(u8, 2048);
     const file_len = try file.readAll(buf);
-    const content = buf[0..file_len];
-    const buffer = try buffer_new(content, allocator);
+    const buffer = try buffer_new(buf[0..file_len], allocator);
 
-    const parser = ts.ts_parser_new();
-    defer ts.ts_parser_delete(parser);
-    var language_lib = try dl.open("/usr/lib/tree_sitter/c.so");
-    var language: *const fn () *ts.struct_TSLanguage = undefined;
-    language = language_lib.lookup(@TypeOf(language), "tree_sitter_c") orelse return error.NoSymbol;
-    _ = ts.ts_parser_set_language(parser, language());
-
-    const tree = ts.ts_parser_parse_string(parser, null, @ptrCast(content), @intCast(content.len));
-    defer ts.ts_tree_delete(tree);
-    const root_node = ts.ts_tree_root_node(tree);
-    std.debug.print("tree: {s}\n", .{@as([*:0]u8, ts.ts_node_string(root_node))});
-
-    const spans = try make_spans(root_node, allocator);
-    // for (spans.items) |span| {
-    //     std.debug.print("{s}\n", .{span.node_type});
-    // }
-
-    std.debug.print("AAAAAA {} {}\n", .{ nc.COLOR_PAIR(1), color_pair.text.to_pair() });
     const win = try init_curses();
     _ = win;
 
-    redraw(&buffer, &spans);
-    _ = nc.move(0, 0);
+    const parser = try init_parser();
+    defer ts.ts_parser_delete(parser);
+
+    try update(parser, &buffer, allocator);
 
     while (true) {
         const key = nc.getch();
         if (key == 'q') {
             _ = nc.endwin();
             return;
+        }
+        if (key == 'i') {
+            action.try_move_cursor(.{ .row = cursor.row - 1, .col = cursor.col });
+        }
+        if (key == 'k') {
+            action.try_move_cursor(.{ .row = cursor.row + 1, .col = cursor.col });
+        }
+        if (key == 'j') {
+            action.try_move_cursor(.{ .row = cursor.row, .col = cursor.col - 1 });
+        }
+        if (key == 'l') {
+            action.try_move_cursor(.{ .row = cursor.row, .col = cursor.col + 1 });
+        }
+        if (need_redraw or need_reparse) {
+            try update(parser, &buffer, allocator);
         }
     }
 }
