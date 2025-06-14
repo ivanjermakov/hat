5010acd99909df3511aedf3ce8a48f56fe5fa7fd
ivanjermakov <ivanjermakov1@gmail.com>
2025-06-14 11:00:16 +0200

    Refactoring

--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,14 @@
 const std = @import("std");
-const posix = std.posix;
-const loc = @cImport({
-    @cInclude("locale.h");
-});
-const termios = @cImport({
-    @cInclude("termios.h");
-});
 const dl = std.DynLib;
-const nc = @cImport({
+const posix = std.posix;
+const c = @cImport({
+    @cInclude("termios.h");
+    @cInclude("locale.h");
     @cInclude("ncurses.h");
 });
-const action = @import("action.zig");
-const input = @import("input.zig");
-const unicode = @import("unicode.zig");
+const act = @import("action.zig");
+const inp = @import("input.zig");
+const uni = @import("unicode.zig");
 const ft = @import("file_type.zig");
 const buf = @import("buffer.zig");
 
@@ -37,7 +33,7 @@ pub const Color = enum(i16) {
     }
 
     pub fn init(self: Color, r: u8, g: u8, b: u8) void {
-        _ = nc.init_color(@intFromEnum(self), rgb_to_curses(r), rgb_to_curses(g), rgb_to_curses(b));
+        _ = c.init_color(@intFromEnum(self), rgb_to_curses(r), rgb_to_curses(g), rgb_to_curses(b));
     }
 };
 
@@ -48,7 +44,7 @@ pub const ColorPair = enum(u8) {
     number,
 
     pub fn init(self: ColorPair, fg: Color, bg: Color) void {
-        _ = nc.init_pair(@intFromEnum(self), @intFromEnum(fg), @intFromEnum(bg));
+        _ = c.init_pair(@intFromEnum(self), @intFromEnum(fg), @intFromEnum(bg));
     }
 
     pub fn to_pair(self: ColorPair) c_int {
@@ -58,7 +54,7 @@ pub const ColorPair = enum(u8) {
 
 const Attr = .{
     .text = ColorPair.text.to_pair(),
-    .keyword = ColorPair.keyword.to_pair() | nc.A_BOLD,
+    .keyword = ColorPair.keyword.to_pair() | c.A_BOLD,
     .string = ColorPair.string.to_pair(),
     .number = ColorPair.number.to_pair(),
 };
@@ -88,14 +84,14 @@ pub var args: Args = .{
     .log = false,
 };
 
-fn init_curses() !*nc.WINDOW {
-    const win = nc.initscr() orelse return error.InitScr;
-    _ = nc.use_default_colors();
-    _ = nc.noecho();
-    _ = loc.setlocale(loc.LC_ALL, "");
+fn init_curses() !*c.WINDOW {
+    const win = c.initscr() orelse return error.InitScr;
+    _ = c.use_default_colors();
+    _ = c.noecho();
+    _ = c.setlocale(c.LC_ALL, "");
 
-    if (nc.has_colors()) {
-        _ = nc.start_color();
+    if (c.has_colors()) {
+        _ = c.start_color();
     }
 
     Color.black.init(0, 0, 0);
@@ -111,13 +107,13 @@ fn init_curses() !*nc.WINDOW {
     ColorPair.string.init(Color.green, Color.none);
     ColorPair.number.init(Color.yellow, Color.none);
 
-    _ = nc.bkgd(@intCast(ColorPair.text.to_pair()));
+    _ = c.bkgd(@intCast(ColorPair.text.to_pair()));
 
     return win;
 }
 
 fn redraw() !void {
-    _ = nc.erase();
+    _ = c.erase();
 
     var byte: usize = 0;
     for (0..buffer.content.items.len) |row| {
@@ -149,29 +145,29 @@ fn redraw() !void {
                     }
                 }
             }
-            _ = nc.attrset(ch_attr);
-            const cchar = unicode.codepoint_to_cchar(ch);
-            _ = nc.mvadd_wch(@intCast(row), @intCast(col), @ptrCast(&cchar));
+            _ = c.attrset(ch_attr);
+            const cchar = uni.codepoint_to_cchar(ch);
+            _ = c.mvadd_wch(@intCast(row), @intCast(col), @ptrCast(&cchar));
             byte += try std.unicode.utf8CodepointSequenceLength(ch);
             col += 1;
         }
         byte += 1;
     }
 
-    _ = nc.standend();
-    _ = nc.move(@intCast(cursor.row), @intCast(cursor.col));
+    _ = c.standend();
+    _ = c.move(@intCast(cursor.row), @intCast(cursor.col));
 
     switch (mode) {
-        .normal => _ = try std_out.write(input.cursor_type.steady_block),
-        .insert => _ = try std_out.write(input.cursor_type.steady_bar),
+        .normal => _ = try std_out.write(inp.cursor_type.steady_block),
+        .insert => _ = try std_out.write(inp.cursor_type.steady_bar),
     }
 }
 
 fn setup_terminal() !void {
-    var tty: termios.struct_termios = undefined;
-    _ = termios.tcgetattr(std.posix.STDIN_FILENO, &tty);
-    tty.c_lflag &= @bitCast(~(termios.ICANON | termios.ECHO));
-    _ = termios.tcsetattr(std.posix.STDIN_FILENO, termios.TCSANOW, &tty);
+    var tty: c.struct_termios = undefined;
+    _ = c.tcgetattr(std.posix.STDIN_FILENO, &tty);
+    tty.c_lflag &= @bitCast(~(c.ICANON | c.ECHO));
+    _ = c.tcsetattr(std.posix.STDIN_FILENO, c.TCSANOW, &tty);
 
     _ = try posix.fcntl(0, posix.F.SETFL, try posix.fcntl(0, posix.F.GETFL, 0) | posix.SOCK.NONBLOCK);
 }
@@ -191,7 +187,7 @@ fn get_codes() !?[]u8 {
     if (log_enabled) {
         std.debug.print("input: ", .{});
         for (in_buf.items) |code| {
-            std.debug.print("{s}", .{try input.ansi_code_to_string(code)});
+            std.debug.print("{s}", .{try inp.ansi_code_to_string(code)});
         }
         std.debug.print("\n", .{});
     }
@@ -199,14 +195,14 @@ fn get_codes() !?[]u8 {
     return in_buf.items;
 }
 
-fn get_keys(codes: []u8) ![]input.Key {
-    var keys = std.ArrayList(input.Key).init(allocator);
+fn get_keys(codes: []u8) ![]inp.Key {
+    var keys = std.ArrayList(inp.Key).init(allocator);
 
     var cs = std.ArrayList(u8).init(allocator);
     try cs.appendSlice(codes);
 
     while (cs.items.len > 0) {
-        const key = input.parse_ansi(&cs) catch |e| {
+        const key = inp.parse_ansi(&cs) catch |e| {
             if (log_enabled) std.debug.print("{}\n", .{e});
             continue;
         };
@@ -246,7 +242,7 @@ pub fn main() !void {
     try redraw();
 
     while (true) {
-        _ = nc.refresh();
+        _ = c.refresh();
         std.time.sleep(sleep_ns);
 
         const codes = try get_codes() orelse continue;
@@ -275,25 +271,25 @@ pub fn main() !void {
                 .insert => {
                     if (code == .escape) mode = .normal;
                     if (code == .delete) {
-                        try action.remove_char();
+                        try act.remove_char();
                         needs_reparse = true;
                     }
                     if (code == .backspace) {
-                        try action.remove_prev_char();
+                        try act.remove_prev_char();
                         needs_reparse = true;
                     }
                     if (code == .enter) {
-                        try action.insert_newline();
+                        try act.insert_newline();
                         needs_reparse = true;
                     }
                     if (key.printable) |printable| {
-                        try action.insert_text(printable);
+                        try act.insert_text(printable);
                         needs_reparse = true;
                     }
                 },
             }
         }
-        action.validate_cursor();
+        act.validate_cursor();
         if (needs_reparse) {
             try buffer.ts_parse();
             try buffer.make_spans();
@@ -305,6 +301,6 @@ pub fn main() !void {
 }
 
 fn deinit() void {
-    _ = nc.endwin();
-    _ = std_out.write(input.cursor_type.steady_block) catch {};
+    _ = c.endwin();
+    _ = std_out.write(inp.cursor_type.steady_block) catch {};
 }
