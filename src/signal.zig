fdee5749e069e14eda546bb6a5d9459f8f993a97
ivanjermakov <ivanjermakov1@gmail.com>
2025-07-22 13:17:54 +0200

    Handle `SIGWINCH`

--- src/signal.zig
+++ src/signal.zig
@@ -1,11 +1,13 @@
 const std = @import("std");
 const log = @import("log.zig");
 const main = @import("main.zig");
+const ter = @import("terminal.zig");
 
 pub fn registerAll() void {
     register(sig_handle.tstp);
     register(sig_handle.cont);
     register(sig_handle.int);
+    register(sig_handle.winch);
 }
 
 fn register(sig: SigHandle) void {
@@ -32,6 +34,7 @@ const sig_handle = .{
     .int = SigHandle{ .sig = std.posix.SIG.INT, .handle = handleSigInt },
     .tstp = SigHandle{ .sig = std.posix.SIG.TSTP, .handle = handleSigTstp },
     .cont = SigHandle{ .sig = std.posix.SIG.CONT, .handle = handleSigCont },
+    .winch = SigHandle{ .sig = std.posix.SIG.WINCH, .handle = handleSigWinch },
 };
 
 fn handleSigInt(sig: c_int) callconv(.C) void {
@@ -55,5 +58,12 @@ fn handleSigCont(sig: c_int) callconv(.C) void {
     register(sig_handle.tstp);
 
     main.term.setup() catch {};
+    main.editor.dirty.draw = true;
+}
+
+fn handleSigWinch(sig: c_int) callconv(.C) void {
+    _ = sig;
+    log.log(@This(), "handling SIGWINCH\n", .{});
+    main.term.dimensions = ter.terminalSize() catch return;
     main.editor.dirty.draw = true;
 }
