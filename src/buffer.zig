828689394831930e627fa304e4b44e993e09770a
ivanjermakov <ivanjermakov1@gmail.com>
2025-08-26 00:55:36 +0200

    Only autosave under git

--- src/buffer.zig
+++ src/buffer.zig
@@ -379,7 +379,7 @@ pub const Buffer = struct {
 
         main.editor.dotRepeatCommitReady();
 
-        if (main.editor.config.autosave) {
+        if (main.editor.config.autosave and self.git_root != null) {
             log.debug(@This(), "autosave {s}\n", .{self.path});
             self.write() catch |e| log.err(@This(), "write buffer error: {}", .{e});
         }
@@ -556,7 +556,7 @@ pub const Buffer = struct {
             }
             self.history_index = if (h_idx > 0) h_idx - 1 else null;
 
-            if (main.editor.config.autosave) {
+            if (main.editor.config.autosave and self.git_root != null) {
                 self.write() catch |e| log.err(@This(), "write buffer error: {}", .{e});
                 log.debug(@This(), "autosave {s}\n", .{self.path});
             }
@@ -576,7 +576,7 @@ pub const Buffer = struct {
         }
         self.history_index = redo_idx;
 
-        if (main.editor.config.autosave) {
+        if (main.editor.config.autosave and self.git_root != null) {
             self.write() catch |e| log.err(@This(), "write buffer error: {}", .{e});
             log.debug(@This(), "autosave {s}\n", .{self.path});
         }
