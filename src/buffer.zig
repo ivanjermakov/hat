69d7e39697c0af559c3441730bf1b17abcbab1b4
ivanjermakov <ivanjermakov1@gmail.com>
2025-09-20 20:35:13 +0200

    Correct indent align of first line in file

--- src/buffer.zig
+++ src/buffer.zig
@@ -847,10 +847,9 @@ pub const Buffer = struct {
     }
 
     fn lineAlignIndent(self: *Buffer, row: usize) !void {
-        if (row == 0) return;
         const old_cursor = self.cursor;
         const line = self.lineContent(row);
-        const correct_indent: usize = self.indents.items[row - 1];
+        const correct_indent: usize = if (row == 0) 0 else self.indents.items[row - 1];
         const correct_indent_spaces = correct_indent * self.file_type.indent_spaces;
         const current_indent_spaces: usize = lineIndentSpaces(line);
         if (correct_indent_spaces == current_indent_spaces) return;
